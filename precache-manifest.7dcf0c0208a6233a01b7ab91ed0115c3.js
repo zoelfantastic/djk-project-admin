60e3dc4eb5e71cc9e9cc42687fea8eae5cd2eaa4
mohammad agus zulfi <mohammad.ardiansyah@infoglobal.co.id>
2020-08-07 16:14:08 +0700

    Updates

---
+++ precache-manifest.7dcf0c0208a6233a01b7ab91ed0115c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c672b107cbdba10693b698905756d8",
+    "url": "/djk-project-admin/index.html"
+  },
+  {
+    "revision": "42eb1d1d956b42fd47be",
+    "url": "/djk-project-admin/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "44a58e27df3ede39dd8a",
+    "url": "/djk-project-admin/static/js/2.d2b245dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/djk-project-admin/static/js/2.d2b245dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42eb1d1d956b42fd47be",
+    "url": "/djk-project-admin/static/js/main.0a793c66.chunk.js"
+  },
+  {
+    "revision": "0198b7339acf84f84056",
+    "url": "/djk-project-admin/static/js/runtime-main.919fa392.js"
+  }
+]);
